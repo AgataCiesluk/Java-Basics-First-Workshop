175647d6103e0f9d2e8d2323dbdd5e59777ebdab
Agata Ciesluk <agata.mikla@gmail.com>
2021-12-11 13:54:06 +0100

    Pom.xml commit

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -3,7 +3,7 @@ package pl.coderslab;
 public class TaskManager {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
 
     }
 }
