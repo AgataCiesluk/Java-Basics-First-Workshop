35c49b8a52ed8d5bcb8f42e1e1c0b195c5b4e8ef
Agata Ciesluk <agata.mikla@gmail.com>
2021-12-11 14:43:32 +0100

    Avbl options

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -4,7 +4,16 @@ package pl.coderslab;
 public class TaskManager {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        availableOptions();
 
+
+    }
+
+    public static void availableOptions() {
+        System.out.println(ConsoleColors.BLUE + "Please select an option: ");
+        String[] options = {"add", "remove", "list", "exit"};
+        for (String option : options) {
+            System.out.println(ConsoleColors.WHITE + option);
+        }
     }
 }
